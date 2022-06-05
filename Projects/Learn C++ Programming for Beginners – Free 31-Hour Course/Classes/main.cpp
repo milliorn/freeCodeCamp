4a0d1b9892c96945d5e2e90aaf436971e1ef8042
milliorn <scottmilliorn@gmail.com>
2022-06-05 13:12:39 -0700

    Setters and Getters

--- Projects/Learn C++ Programming for Beginners – Free 31-Hour Course/Classes/main.cpp
+++ Projects/Learn C++ Programming for Beginners – Free 31-Hour Course/Classes/main.cpp
@@ -5,7 +5,7 @@ const double PI = 3.1415926535;
 class Cylinder
 {
 public:
-  Cylinder() = default;
+  // Cylinder() = default;
   Cylinder()
   {
     base_radius = 2.0;
@@ -23,6 +23,26 @@ public:
     return PI * base_radius * base_radius * base_height;
   }
 
+  double get_base_radius()
+  {
+    return base_radius;
+  }
+
+  double get_height()
+  {
+    return base_height;
+  }
+
+  void set_base_radius(double radius)
+  {
+    base_radius = radius;
+  }
+
+  void set_height(double height)
+  {
+    base_height = height;
+  }
+
 private:
   double base_radius;
   double base_height;
@@ -32,5 +52,8 @@ int main()
 {
   Cylinder cylinder;
   std::cout << "volume -> " << cylinder.volume() << std::endl;
+  std::cout << "radius -> " << cylinder.get_base_radius() << std::endl;
+  std::cout << "height -> " << cylinder.get_height() << std::endl;
+
   return 0;
 }
--- Projects/Learn C++ Programming for Beginners – Free 31-Hour Course/readme.md
+++ Projects/Learn C++ Programming for Beginners – Free 31-Hour Course/readme.md
@@ -8,9 +8,8 @@
 
 ⭐️ Course Contents ⭐
 
-<https://youtu.be/8jLOx1hD3_o?t=75582>
+<https://youtu.be/8jLOx1hD3_o?t=76207>
 
-Setters and Getters
 Class Across Multiple Files
 Arrow pointer call notation
 Destructors
