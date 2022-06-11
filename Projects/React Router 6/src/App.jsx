9a3a63bac5a8c6be02f01a147307503888617a50
milliorn <scottmilliorn@gmail.com>
2022-06-10 22:15:19 -0700

    Error

--- Projects/React Router 6/src/App.jsx
+++ Projects/React Router 6/src/App.jsx
@@ -2,15 +2,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
+import Error from "./pages/Error";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="*" element={<Error />} />
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
         <Route path="products" element={<Products />} />
-
         <Route
           path="testing"
           element={
