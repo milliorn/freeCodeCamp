d2707202ca963fd660103805063f531a959586d5
milliorn <scottmilliorn@gmail.com>
2022-06-15 19:32:25 -0700

    Name Resources in Plural

--- Projects/REST API Design Best Practices Handbook/crossfit-wod-api/src/index.js
+++ Projects/REST API Design Best Practices Handbook/crossfit-wod-api/src/index.js
@@ -1,13 +1,10 @@
-/* Configure Express */
+const v1WorkoutRouter = require("./v1/routes/workoutRoutes"); /* Configure Express */
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use("/api/v1/Workouts", v1WorkoutRouter);
+
 app.listen(PORT, () => {
   console.log(`API is listening on port ${PORT}`);
-});
-
-/* testing */
-app.get("/", (req, res) => {
-  res.send("<h2>Hello World!</h2>");
 });
