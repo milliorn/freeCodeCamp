b44c454c983b34270dce60c1c36ee51e77c39643
milliorn <scottmilliorn@gmail.com>
2022-06-16 18:08:12 -0700

    post data to database

--- Projects/How to Build a RESTful API Using Node, Express, and MongoDB/routes/routes.js
+++ Projects/How to Build a RESTful API Using Node, Express, and MongoDB/routes/routes.js
@@ -3,10 +3,6 @@ const express = require("express");
 
 const router = express.Router();
 
-router.post("/post", (req, res) => {
-  res.send("Post API");
-});
-
 router.get("/getAll", (req, res) => {
   res.send("Get All API");
 });
@@ -21,6 +17,20 @@ router.patch("/update/:id", (req, res) => {
 
 router.delete("/delete/:id", (req, res) => {
   res.send("Delete by ID API");
+});
+
+router.post("/post", async (req, res) => {
+  const data = new Model({
+    name: req.body.name,
+    age: req.body.age,
+  });
+
+  try {
+    const dataToSave = await data.save();
+    res.status(200).json(dataToSave);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
 
 module.exports = router;
