72652f56f5ffc0b094dd879b365cb21173b0df05
milliorn <scottmilliorn@gmail.com>
2022-06-16 17:42:58 -0700

    Endpoints

--- Projects/How to Build a RESTful API Using Node, Express, and MongoDB/routes/routes.js
+++ Projects/How to Build a RESTful API Using Node, Express, and MongoDB/routes/routes.js
@@ -1,4 +1,24 @@
 const express = require("express");
 const router = express.Router();
 
+router.post("/post", (req, res) => {
+  res.send("Post API");
+});
+
+router.get("/getAll", (req, res) => {
+  res.send("Get All API");
+});
+
+router.get("/getOne/:id", (req, res) => {
+  res.send(req.params.id);
+});
+
+router.patch("/update/:id", (req, res) => {
+  res.send("Update by ID API");
+});
+
+router.delete("/delete/:id", (req, res) => {
+  res.send("Delete by ID API");
+});
+
 module.exports = router;
