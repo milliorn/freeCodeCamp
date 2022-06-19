1c3d40e3b8bdba88ae5675ff2ae3db448faddc43
milliorn <scottmilliorn@gmail.com>
2022-06-19 12:06:05 -0700

    CharacterViewer

--- Projects/How to Build a Full Stack RPG Character/client/src/App.vue
+++ Projects/How to Build a Full Stack RPG Character/client/src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <CharacterViewer />
+    <CharacterCreator />
   </div>
 </template>
 
 <script>
 import CharacterViewer from './components/CharacterViewer.vue'
+import CharacterCreator from './components/CharacterCreator.vue'
 
 export default {
   name: 'App',
   components: {
-    CharacterViewer
+    CharacterViewer,
+    CharacterCreator
   }
 }
 </script>
