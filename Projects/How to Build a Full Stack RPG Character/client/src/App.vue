e20311e579d1cde6fba3efa587d53238882beb8c
milliorn <scottmilliorn@gmail.com>
2022-06-19 12:04:14 -0700

    The Front End

---
+++ Projects/How to Build a Full Stack RPG Character/client/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <CharacterViewer />
+  </div>
+</template>
+
+<script>
+import CharacterViewer from './components/CharacterViewer.vue'
+
+export default {
+  name: 'App',
+  components: {
+    CharacterViewer
+  }
+}
+</script>
---
+++ Projects/How to Build a Full Stack RPG Character/readme.md
@@ -0,0 +1,4 @@
+# How to Build a Full Stack RPG Character Generator with MongoDB, Express, Vue, and Node (the MEVN Stack)
+
+<https://www.freecodecamp.org/news/build-a-full-stack-mevn-app/>
+
