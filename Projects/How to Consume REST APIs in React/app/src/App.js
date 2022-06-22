c2085b5e5c7046f676babeeb2b2f2266e2924bee
milliorn <scottmilliorn@gmail.com>
2022-06-21 19:44:40 -0700

    POST

--- Projects/How to Consume REST APIs in React/app/src/App.js
+++ Projects/How to Consume REST APIs in React/app/src/App.js
@@ -5,6 +5,8 @@ import { useState, useEffect } from "react";
 function App() {
   /* create state to store data */
   const [posts, setPosts] = useState([]);
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
 
   const endpoint = "https://jsonplaceholder.typicode.com/posts?_limit=10";
 
@@ -22,21 +24,63 @@ function App() {
       });
   }, []);
 
+  /* POST method to send data from an endpoint. */
+  const addPosts = async (title, body) => {
+    await fetch(endpoint, {
+      method: "POST",
+      body: JSON.stringify({
+        /*The body holds the data we want to pass into the API,
+        which we must first stringify because we are sending data to a web server */
+        title: title,
+        body: body,
+        userId: Math.random().toString(36).slice(2),
+      }),
+      headers: {
+        /*The header tells us the type of data, which is always the same when consuming REST API's. */
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        /* Set the state to hold the new data and distribute the remaining data into the array. */
+        setPosts((posts) => [data, ...posts]);
+        setTitle("");
+        setBody("");
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addPosts(title, body);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="add-post-container">
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              className="form-control"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+            <textarea
+              name=""
+              className="form-control"
+              id=""
+              cols="1"
+              rows="1"
+              value={body}
+              onChange={(e) => setBody(e.target.value)}
+            ></textarea>
+            <button type="submit">Add Post</button>
+          </form>{" "}
+        </div>
       </header>
     </div>
   );
