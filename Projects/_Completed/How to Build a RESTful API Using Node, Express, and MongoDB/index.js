2f7318359439a74e54fa32f7538550033f0f1dc7
milliorn <scottmilliorn@gmail.com>
2022-06-23 13:17:28 -0700

    clear

--- Projects/_Completed/How to Build a RESTful API Using Node, Express, and MongoDB/index.js
+++ Projects/_Completed/How to Build a RESTful API Using Node, Express, and MongoDB/index.js
@@ -1,10 +1,13 @@
+/*import the contents of our .env file */
 require("dotenv").config();
 
+/* Connect the database to our server using Mongoose */
 const mongoose = require("mongoose");
 const mongoString = process.env.DATABASE_URL;
 mongoose.connect(mongoString);
 const database = mongoose.connection;
 
+/* Throw a success or an error message if database connection is successful or fails. */
 database.on("error", (error) => {
   console.log(error);
 });
@@ -15,10 +18,16 @@ database.once("connected", () => {
 
 const express = require("express");
 const app = express();
-const routes = require("./routes/routes");
 
-app.use(express.json());
+/*
+Create our Routes for the Endpoints
+localhost:3000/api/
+*/
+const routes = require("./routes/routes");
 app.use("/api", routes);
+app.use(express.json());
+
+/* Listen for changes of this file. */
 app.listen(3000, () => {
   console.log(`Server Started at ${3000}`);
 });
